39cd76a4f7b2b7ff804976f528279b112d9dfcf5
Ikhwanul Abiyu Dhiyya'ul Haq <86358803+Soellassy24@users.noreply.github.com>
2022-05-06 22:13:21 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,20 @@
 using namespace std;
 
 void endProgram(){
-    cout << endl;
-    cout << "Press Enter to Continue...";
+    system("cls || clear");
+    cout << "Thanks for using this program! Press enter to exit program";
     getch();
 }
-
 int main(){
+    int pilihan;
+    const char *index[4] = {"xMin", "xMaks", "yMin", "yMaks"};
     float panjang, lebar, tx, ty;
     persegiPanjang pp3(0,0,0,0);
 
     cout << "-----------------Tugas Operator Overloading-----------------" << endl;
+    cout << "----------------IKHWANUL ABIYU DHIYYA'UL HAQ----------------" << endl;
+    cout << "-------------------------5024211048-------------------------" << endl;
+    cout << "--------------------TEKNIK KOMPUTER 2021--------------------" << endl;
     cout << "(Dibuat untuk memenuhi tugas Mata Kuliah Pemrograman Lanjut)" << endl << endl;
 
     cout << "Masukkan Data Persegi Panjang 1" << endl;
@@ -32,61 +36,139 @@ int main(){
 
     system("cls || clear");
 
-    cout << "Data Awal" << endl;
-    cout << "PERSEGI PANJANG 1" << endl; 
-    pp1.output();
-    cout << endl;
-    cout << "PERSEGI PANJANG 2" << endl;
-    pp2.output();
+    while (true){
+        cout << "Data Awal" << endl;
+        cout << "PERSEGI PANJANG 1" << endl; 
+        pp1.output();
+        cout << endl;
+        cout << "PERSEGI PANJANG 2" << endl;
+        pp2.output();
+        cout << endl << endl;
+        cout << "Silahkan pilih operator yang ingin digunakan" << endl;
+        cout << "1. OPERATOR +" << endl;
+        cout << "2. OPERATOR -" << endl;
+        cout << "3. OPERATOR ++" << endl;
+        cout << "4. OPERATOR --" << endl;
+        cout << "5. OPERATOR []" << endl;
+        cout << "6. OPERATOR ==" << endl;
+        cout << "7. Exit Program" << endl;
+        cout << "Pilihan anda : "; cin >> pilihan;
+        switch (pilihan){
+        case 1:
+            {
+                system("cls || clear");
+                cout << endl;
+                cout << "OPERATOR + (Mengambil luas gabungan dari 2 persegi panjang)" << endl;
+                pp3 = pp1 + pp2;
+                if (pp1==pp2){
+                    pp3.output();
+                }
+                cout << "Press enter to continue..." << endl;
+                getch();
+                system("cls || clear");
+            }
+            break;
+        case 2:
+            {
+                system("cls || clear");
+                cout << endl;
+                cout << "OPERATOR - (Mengambil irisan 2 persegi panjang)" << endl;
+                pp3 = pp1 - pp2;
+                if (pp1==pp2){
+                    pp3.output();
+                }
+                cout << "Press enter to continue..." << endl;
+                getch();
+                system("cls || clear");
+            }
+            break;
+        case 3:
+            {
+                system("cls || clear");
+                cout << endl;
+                cout << "OPERATOR ++ (Menjadikan luas persegi panjang 2 kali lebih besar)" << endl;
+                cout << "Masukkan Persegi Panjang yang ingin dikenakan operator ++ : "; cin >> pilihan; cout << endl;
+                if (pilihan == 1){
+                    ++pp1;
+                    cout << "Persegi Panjang 1 setelah diberi Operator ++ : " << endl;
+                    pp1.output();
+                    --pp1;
+                }else if(pilihan == 2){
+                    ++pp2;
+                    cout << "Persegi Panjang 2 setelah diberi Operator ++ : " << endl;
+                    pp2.output();
+                    --pp2;
+                }
+                cout << "Press enter to continue..." << endl;
+                getch();
+                system("cls || clear");
+            }
+            break;
+        case 4:
+            {
+                system("cls || clear");
+                cout << endl;
+                cout << "OPERATOR -- (Menjadikan luas persegi panjang 2 kali lebih kecil (1/2 kali semula))" << endl;
+                cout << "Masukkan Persegi Panjang yang ingin dikenakan operator -- : "; cin >> pilihan; cout << endl;
+                if (pilihan == 1){
+                    --pp1;
+                    cout << "Persegi Panjang 1 setelah diberi Operator -- : " << endl;
+                    pp1.output();
+                    ++pp1;
+                }else if(pilihan == 2){
+                    --pp2;
+                    cout << "Persegi Panjang 2 setelah diberi Operator -- : " << endl;
+                    pp2.output();
+                    ++pp2;
+                }
+                cout << "Press enter to continue..." << endl;
+                getch();
+                system("cls || clear");
+            }
+            break;
+        case 5:
+            {
+                system("cls || clear");
+                cout << endl;
+                cout << "OPERATOR [] (Perbandingan indeks Pp1 dan Pp2)" << endl;
+                cout << "1. xMin" << endl;
+                cout << "2. xMaks" << endl;
+                cout << "3. yMin" << endl;
+                cout << "4. yMaks" << endl;
+                cout << "Masukkan operator yang ingin dibandingkan : "; cin >> pilihan; cout << endl;
 
-    cout << endl;
-    cout << "OPERATOR +" << endl;
-    pp3 = pp1 + pp2;
-    if (pp1==pp2){
-        pp3.output();
+                cout << "Nilai " << index[pilihan-1] << " Pp1 : " << pp1[pilihan] << endl;
+                cout << "Nilai " << index[pilihan-1] << " Pp2 : " << pp2[pilihan] << endl;
+                if (pp1[pilihan] > pp2[pilihan]){
+                    cout << index[pilihan-1] << " Pp1 lebih besar dari " << index[pilihan-1] << " Pp2" << endl;
+                }else if(pp2[pilihan] > pp1[pilihan]){
+                    cout << index[pilihan-1] << " Pp2 lebih besar dari " << index[pilihan-1] << " Pp1" << endl;;
+                }else{
+                    cout << "Kedua " << index[pilihan-1] << " sama" << endl;
+                }
+                cout << "Press enter to continue..." << endl;
+                getch();
+                system("cls || clear");
+            }
+            break;
+        case 6:
+            {
+                system("cls || clear");
+                cout << endl;
+                cout << "OPERATOR == (Pembuktian Kedua Persegi panjang saling beririsan/tidak)" << endl;
+                if (pp1==pp2){
+                    cout << "Kedua persegi panjang beririsan" << endl;
+                }else{
+                    cout << "Kedua persegi panjang tidak beririsan" << endl;
+                }
+                cout << "Press enter to continue..." << endl;
+                getch();
+                system("cls || clear");
+            }
+            break;
+        case 7:
+            endProgram();
+            return 0;
+        }
     }
-    
-    cout << endl;
-    cout << "OPERATOR -" << endl;
-    pp3 = pp1 - pp2;
-    if (pp1==pp2){
-        pp3.output();
-    }
-    
-    cout << endl;
-    cout << "OPERATOR ++ (Pada persegi panjang 1)" << endl;
-    ++pp1;
-    cout << "Persegi Panjang 1 setelah diberi Operator ++ : " << endl;
-    pp1.output();
-    --pp1;
-
-    cout << endl;
-    cout << "OPERATOR -- (Pada persegi panjang 2)" << endl;
-    --pp2;
-    cout << "Persegi Panjang 2 setelah diberi Operator -- : " << endl;
-    pp2.output();
-    ++pp2;
-
-    cout << endl;
-    cout << "OPERATOR [] (Perbandingan kedua xMaks Pp1 dan Pp2)" << endl;
-    cout << "Nilai xMaks Pp1 : " << pp1[2] << endl;
-    cout << "Nilai xMaks Pp2 : " << pp2[2] << endl;
-    if (pp1[2] > pp2[2]){
-        cout << "xMaks Pp 1 lebih besar dari xMaks Pp 2";
-    }else if(pp2[2] > pp1[2]){
-        cout << "xMaks Pp 2 lebih besar dari xMaks Pp 1";
-    }else{
-        cout << "Kedua xMaks sama";
-    }
-
-    cout << endl << endl;
-    cout << "OPERATOR == (Pembuktian Kedua Persegi panjang saling beririsan/tidak)" << endl;
-    if (pp1==pp2){
-        cout << "Kedua persegi panjang beririsan" << endl;
-    }else{
-        cout << "Kedua persegi panjang tidak beririsan" << endl;
-    }
-    
-    endProgram();
-    return 0;
 }
